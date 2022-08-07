49ab1f538e493ab4e2a1bfc336b55ff656daf3ec
tholeb <43311443+tholeb@users.noreply.github.com>
2022-08-07 15:42:47 +0200

    Add: option to get rid of the dlc name from the ped name

--- util.py
+++ util.py
@@ -6,12 +6,13 @@ from shutil import copy
 
 from shared.functions import *
 
-parser = argparse.ArgumentParser(
+p = argparse.ArgumentParser(
     description='''move a group to it's corresponding output folder \033[31mYou must do it group by group (jbib, accs, etc.).\033[0m''',
     epilog="""Written by tholeb <tholeb.fr>""")
-parser.add_argument('--input', type=str, default='./input', help='The input folder. Default to "./input"')
-parser.add_argument('--output', type=str, default='./output', help='The input folder. Default to "./output"')
-args = parser.parse_args()
+p.add_argument('-i', '--input', type=str, default='./input', help='The input folder. Default to "./input"')
+p.add_argument('-o', '--output', type=str, default='./output', help='The input folder. Default to "./output"')
+p.add_argument('--dlc', action='store_true', help='Attempt to get rid of DLC name from a ped (e.g. mp_f_freemode_01_mp_f_bikerdlc_01 to mp_f_freemode_01)')
+args = p.parse_args()
 
 # Get all .ydd and .ytd files in the input folder
 input = glob(f'{args.input}/*.ydd') + glob(f'{args.input}/*.ytd')
@@ -37,6 +38,15 @@ for i, path in enumerate([f for f in input if f.endswith('.ydd')]):
         print(f"\033[41m{path} is neither a component ({', '.join(components)}) nor a prop ({', '.join(props)}) -- IGNORED\033[0m")
         continue
 
+    # Attempt to get rid of the DLC name
+    if args.dlc and ped.startswith('mp_'):
+        p = ped.split('_')
+
+        if outfit.split('_')[0] in components:
+            ped = '_'.join(p[:4])
+        elif outfit.split('_')[1] in props:
+            ped = '_'.join(p[:5])
+
     # Create components/props folders
     ped_dir = f"{args.output}/{ped}"
     if ped.endswith('_p'):
@@ -45,6 +55,7 @@ for i, path in enumerate([f for f in input if f.endswith('.ydd')]):
         # Get the model type and number
         type, number = outfit.split('_')[1], outfit.split('_')[2]
 
+        # Create the dirs where we'll store each prop
         dir = f"{ped_dir}/props"
         [makedirs(f"{dir}/{prop}", exist_ok=True) for prop in props]
     else:
@@ -53,18 +64,24 @@ for i, path in enumerate([f for f in input if f.endswith('.ydd')]):
         # Get the model type and number
         type, number = outfit.split('_')[0], outfit.split('_')[1]
 
+        # Create the dirs where we'll store each component
         dir = f"{ped_dir}/components"
         [makedirs(f"{dir}/{component}", exist_ok=True) for component in components]
 
+    # The folder's number (each component/prop need to have a single number for each type. e.g 0.ydd)
     num = len(glob(f"{dir}/{type}/*.ydd"))
 
+    # Copy the model to the appropriate folder and create it's textures folder
     makedirs(f"{dir}/{type}/{num}", exist_ok=True)
     copy(path, f"{dir}/{type}/{num}.ydd")
 
+    # Get all the textures for a matching model
     textures = [t for t in input if f"{type}_diff_{number}" in t]
 
+    # And copy them to the appropriate folder
     [copy(v, f"{dir}/{type}/{num}/{k}.ytd") for k, v in enumerate(textures)]
 
-    # Print all the info
+    # Finally, Print some the info
     print(f"\033[35m#{i}\033[0m \033[96m{path}\033[0m --> \033[32m{dir}/{type}/{num}.ydd\033[0m\n\
-            Textures --> \033[34m{dir}/{type}/{num}/*.ytd\033[0m \033[33m({len(textures)})\033[0m \n")
+            Textures --> \033[34m{dir}/{type}/{num}/*.ytd\033[0m \033[33m({len(textures)})\033[0m \n\
+            Ped --> \033[34m{ped}\033[0m")
